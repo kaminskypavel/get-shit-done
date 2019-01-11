dd31c35b8a6d382b0ad5517ebfa69351a1be6d2c
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-01-11 20:06:15 +0200

    chore: improve prettier configs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import './App.scss';
-import logo from './logo.svg';
+import React, {Component} from "react";
+import "./App.scss";
+import logo from "./logo.svg";
 
 class App extends Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
