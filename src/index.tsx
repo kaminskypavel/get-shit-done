664501e11de369672abd56a7bc04c6862da277a1
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-01-20 16:10:23 +0200

    feat: add GA

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import './index.scss';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./index.scss";
+import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize("UA-67512414-4");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+ReactDOM.render(<App/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
