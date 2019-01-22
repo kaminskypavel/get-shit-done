f2d331f79ec3f467d3106e6b8a5c7aa822c25796
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-01-22 10:42:03 +0200

    feat: show a pre close alert

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,17 @@ import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import ReactGA from "react-ga";
 
+// analytics
 ReactGA.initialize("UA-67512414-4");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
+// pre tab close hook
+window.addEventListener("beforeunload", (ev) => {
+  ev.preventDefault();
+  return ev.returnValue = "Are you sure you want to close?";
+});
+
+// lets rock
 ReactDOM.render(<App/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
