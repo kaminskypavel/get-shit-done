17098dc94fd4f2bc0e205f8631f4c3c399771223
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-01-22 10:51:05 +0200

    fix: show a pre close alert only in prod

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,19 @@ import App from "./components/App/App";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import ReactGA from "react-ga";
+import {isProduction} from "./utils";
 
 // analytics
 ReactGA.initialize("UA-67512414-4");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 // pre tab close hook
-window.addEventListener("beforeunload", (ev) => {
-  ev.preventDefault();
-  return ev.returnValue = "Are you sure you want to close?";
-});
+if (isProduction()) {
+  window.addEventListener("beforeunload", (ev) => {
+    ev.preventDefault();
+    return ev.returnValue = "Are you sure you want to close?";
+  });
+}
 
 // lets rock
 ReactDOM.render(<App/>, document.getElementById("root"));
